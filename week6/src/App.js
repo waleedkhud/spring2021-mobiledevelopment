c07cfa53d8f055a28e4d7681a61e1abd0ad538a0
waleedkhud <77756665+waleedkhud@users.noreply.github.com>
2021-02-19 11:38:00 -0500

    Week 6

---
+++ week6/src/App.js
@@ -0,0 +1,9 @@
+import Example from './components/example/index.js';
+
+function App() {
+  return (
+ <Example/>
+  );
+}
+
+export default App;
---
+++ week6/src/components/example/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+    class Example extends React.Component {
+        render(){
+            return <p> My name is Alwaleed</p>
+        }
+    }
+export default Example
